155263adcc14c10785fb457b5f7b95a4ba866258
klimacka <klimu.jana@gmail.com>
2023-09-17 12:07:56 +0200

    Add MUI

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,19 @@
 import react from "@vitejs/plugin-react";
 import { UserConfig } from "vite";
+import htmlPlugin from "vite-plugin-html-config";
 import ssr from "vite-plugin-ssr/plugin";
 
+const htmlConfig = {
+  links: [
+    {
+      rel: "stylesheet",
+      href: "https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;600;700&display=swap",
+    },
+  ],
+};
+
 const config: UserConfig = {
-  plugins: [react(), ssr()],
+  plugins: [react(), ssr(), htmlPlugin(htmlConfig)],
 };
 
 export default config;
